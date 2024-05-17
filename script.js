1d7544c30a48ab865c9b4f10ba6ab78863d1313e
Joaquin <j.ampuero.cacic@gmail.com>
2024-05-16 21:20:16 -0500

    basic html and script for the random rock paper scissors pc logic

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello")
+// create a rule to select rock paper or scissors based on numbers from 1 to 100 for the machine// 
+function getcomputerchoice() {
+    if Math.random() * 100 <33 {
+        console.log("Rock")
+    } else if Math.random * 100 < 66 {
+        console.log("Paper")
+    } else {
+        console.log("Scissors")
+    }
+}
+
+
+
+// create a prompt thing to let people input their choice , limit it so it can only be rock paper or tijeras//
+// keep track of the score, maybe create some variables that store scores or one variable that adds up for each player (the machine and the person)
+// create the logic for the game, who wins and who loses , a lot of if if if//
+// create a function that makes the game repeat itself 5 times and accumulate scores//
+
